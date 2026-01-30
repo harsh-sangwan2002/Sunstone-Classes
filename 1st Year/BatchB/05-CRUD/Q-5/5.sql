ed1fad2adc26ae791976c1147f50e007813e1f77
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-01-30 14:23:34 +0530

    CRUD-05 questions discussed

---
+++ 1st Year/BatchB/05-CRUD/Q-5/5.sql
@@ -0,0 +1,29 @@
+/*
+Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring".
+
+Return the result table ordered by rating in descending order and id in ascending order.
+
+Note: Use the mod(column_name,number) function to find out an odd-numbered or even-numbered ID. 
+*/
+table name -> movies
+columns -> id, description, rating
+
+Select
+    *
+From
+    movies 
+Where
+    id%2 != 0 and 
+    description != "boring"
+Order by
+    rating desc;
+
+Select
+    *
+From
+    movies 
+Where
+    mod(id,2) != 0 and 
+    description != "boring"
+Order by
+    rating desc;

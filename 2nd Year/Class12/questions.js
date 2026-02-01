59100641205863a646a0266b41cb3a6ed9dd94ef
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-01 19:32:57 +0530

    added questions for 2nd year

---
+++ 2nd Year/Class12/questions.js
@@ -0,0 +1,13 @@
+const users = [
+    { firstName: 'John', lastName: 'Doe', age: 25 },
+    { firstName: 'Jane', lastName: 'Smith', age: 20 },
+    { firstName: 'Jim', lastName: 'Brown', age: 25 },
+    { firstName: 'Jake', lastName: 'White', age: 28 },
+    { firstName: 'Jill', lastName: 'Black', age: 20 },
+]
+
+// Q-1: List of full names
+
+// Q-2: {20:2, 25:2, 28:1}
+
+// Q:3 - Find names of users whose age is less than 30

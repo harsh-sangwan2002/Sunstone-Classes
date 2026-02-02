8c8514963c24affba78f48993f5ddb105f93f8ca
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-02 14:43:16 +0530

    removed extra classes

--- 1st Year/BatchA/06-CRUD/Q-1/1.sql
+++
@@ -1,7 +0,0 @@
-/*
-Write an SQL query to find the ids of products that are both low fat and recyclable.
-Return the result table sorted by product_id in ascending order
-The query result format is in the following example.
-*/
-table name -> products 
-columns -> product_id, low_fat, recyclable
--- 1st Year/BatchC/06-CRUD/Q-5/10.sql
+++
@@ -1,9 +0,0 @@
-/*
-Consider a database schema for movies with the following table:
-
-Movies: movie_id (Integer, Primary Key), movie_title (Text), genre (Text)
-
-Write an SQL query to retrieve the titles of movies that are not in the genres "Romance" or "Drama."
-
-Return the output ordered by movie_id in ascending order.
-*/

cf226302a0e039fec9df97f46d3ac08035d56d43
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-24 16:06:41 +0530

    Batch C - Class 13 Joins done

---
+++ 1st Year/BatchC/13-JOINS/03-Article-Views/3.sql
@@ -0,0 +1,18 @@
+/*
+Write a query to find all the people who viewed more than one article on the same date.
+
+Save the viewer_id as the id.
+Return the result sorted by id in ascending order.
+*/
+Select
+    distinct v1.viewer_id id
+From
+    views v1
+Join
+    views v2
+On
+    v1.viewer_id = v2.viewer_id and
+    v1.view_date = v2.viewer_date and
+    v1.article_id != v2.article_id
+Order By
+    id;

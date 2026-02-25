56de672082ac7aeb71b872fe82f49054e00c9974
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-25 12:27:08 +0530

    Batch C - Class 13 Joins questions discussed

--- 1st Year/BatchC/13-JOINS/03-Article-Views/3.sql
+++ 1st Year/BatchC/13-JOINS/03-Article-Views/3.sql
@@ -12,7 +12,7 @@ Join
     views v2
 On
     v1.viewer_id = v2.viewer_id and
-    v1.view_date = v2.viewer_date and
+    v1.view_date = v2.view_date and
     v1.article_id != v2.article_id
 Order By
     id;

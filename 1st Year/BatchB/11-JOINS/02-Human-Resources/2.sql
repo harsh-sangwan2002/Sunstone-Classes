1fd7f5a50cbe52ec9f7889b5ff119bb9d3488f4d
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-25 11:28:28 +0530

    Class 11 - Joins questions discussed

---
+++ 1st Year/BatchB/11-JOINS/02-Human-Resources/2.sql
@@ -0,0 +1,22 @@
+/*
+Write a query to find details of the employees who work in the 'Human Resources' department.
+
+Return the columns 'employee_id', 'department_id', 'first_name', 'last_name', 'job_id', and 'department_name'.
+Return the result ordered by employee_id in ascending order.
+*/
+Select
+    e.employee_id,
+    e.department_id,
+    e.first_name,
+    e.last_name,
+    e.job_id,
+    d.department_name
+From
+    Employees e
+Join
+    Departments d
+On
+    e.department_id = d.department_id and
+    department_name = "Human Resources"
+Order By
+    e.employee_id;

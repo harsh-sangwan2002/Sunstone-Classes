204636518cce861d1106c1f0354ebbca07ae4fd6
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-26 10:27:29 +0530

    Batch C - Class 14 Joins questions discussed

---
+++ 1st Year/BatchC/14-JOINS/04-US-Departments/4.sql
@@ -0,0 +1,29 @@
+/* 
+Display the details of those employees who have a manager working in the department that is US (i.e., country_id) based.
+
+Note:
+
+Return the columns employee_id, first_name, and last_name.
+Return the result ordered by employee_id in ascending order. 
+*/
+Select
+    e.employee_id,
+    e.first_name,
+    e.last_name
+From
+    Employees e
+Join
+    Employees m
+On
+    e.manager_id = m.employee_id
+Join
+    Departments d
+On
+    m.department_id = d.department_id
+Join
+    Locations l
+On
+    d.location_id = l.location_id and
+    l.country_id = "US"
+Order By
+    e.employee_id;

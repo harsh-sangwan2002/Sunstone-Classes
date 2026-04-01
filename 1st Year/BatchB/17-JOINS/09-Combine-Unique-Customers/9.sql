d86189f1eedc4d5760aa00e98f81a949ca8f14dd
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-04-01 11:18:44 +0530

    Class 17 - JOINS done

---
+++ 1st Year/BatchB/17-JOINS/09-Combine-Unique-Customers/9.sql
@@ -0,0 +1,9 @@
+/*
+Write an SQL query to combine all unqiue entries from Customers1 and Customers2.
+
+Return the output ordered by customer_name in descending order. 
+*/
+Select * From Customers1
+Union
+Select * From Customers2
+Order By customer_name;
